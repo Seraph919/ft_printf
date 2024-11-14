dc6cec850c0364751f76e318a13d66338d8dca26
Seraph <167767889+Seraph919@users.noreply.github.com>
2024-11-14 16:09:13 +0100

    Add files via upload

--- p_memory.c
+++ p_memory.c
@@ -6,38 +6,47 @@
 /*   By: asoudani <asoudani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 17:36:26 by asoudani          #+#    #+#             */
-/*   Updated: 2024/11/13 23:29:28 by asoudani         ###   ########.fr       */
+/*   Updated: 2024/11/14 16:04:40 by asoudani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void    p_memory(void *ad)
+static void	splitting_f(char *adress, char *hex, size_t n, int *nb)
 {
-    size_t      n;
-    int         i;
-    char        *hex;
-    char        *adress;
-    
-    adress = malloc(18);
-    if (!adress)
-        return ;
-    hex = "0123456789abcdef";
-    n = (size_t) ad;
-    i = 2;
-    ft_putstr_fd("0x", 1);
-    if (n == 0)
-        write(1, "0", 1);
-    else
-    {
-        while (n > 0)
-        {
-            adress[i] = hex[n%16];
-            n /= 16;
-            i++;
-        }
-        while (--i)
-            ft_putchar_fd(adress[i], 1);
-    }
-    free(adress);
+	int	i;
+
+	i = 1;
+	while (n > 0)
+	{
+		adress[i] = hex[n % 16];
+		n /= 16;
+		i++;
+	}
+	while (--i)
+		ft_putchar_fd(adress[i], 1, nb);
+}
+
+void	p_memory(void *ad, int *nb)
+{
+	size_t	n;
+	char	*hex;
+	char	*adress;
+
+	adress = malloc(18);
+	if (!adress)
+		return ;
+	hex = "0123456789abcdef";
+	n = (size_t)ad;
+	ft_putstr_fd("0x", 1, nb);
+	if (n == 0)
+	{
+		write(1, "0", 1);
+		*nb += 1;
+	}
+	else
+	{
+		splitting_f(adress, hex, n, nb);
+	}
+	free(adress);
 }
